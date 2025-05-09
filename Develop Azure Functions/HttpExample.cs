f081aed03700ea8bcffe0333b297800ae4f81ea6
Eric Slutz <eric.slutz@icloud.com>
2025-05-09 10:56:39 -0400

    Update function

--- Develop Azure Functions/HttpExample.cs
+++ Develop Azure Functions/HttpExample.cs
@@ -3,21 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace Demo.Function;
+namespace Develop_Azure_Functions;
 
-public class HttpExample
+public class HttpExample(ILogger<HttpExample> logger)
 {
-    private readonly ILogger<HttpExample> _logger;
-
-    public HttpExample(ILogger<HttpExample> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<HttpExample> _logger = logger;
 
     [Function("HttpExample")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        _logger.LogInformation("HTTP trigger function processed a request. Method: {Method}, Path: {Path}", req.Method, req.Path);
+        return new OkObjectResult("Well, you're here... Now what? ¯\\_(ツ)_/¯");
     }
 }
